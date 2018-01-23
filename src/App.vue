f098eeaf69ad65d3527cd43440e67651aacb2df8
Dillion <dillion@Dillions-iMac.local>
2018-01-23 17:14:40 -0600

    updates made up until the editing additions

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+   <div id="app">
+      <todo-list v-bind:todos="todos"></todo-list>
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import TodoList from './components/TodoList';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+   name: 'App',
+   components: {
+      TodoList,
+   },
+   data() {
+      return {
+         todos: [{
+            tite: 'Todo A',
+            project: 'Project A',
+            done: false,
+         }, {
+            tite: 'Todo b',
+            project: 'Project b',
+            done: true,
+         }, {
+            tite: 'Todo c',
+            project: 'Project c',
+            done: true,
+         }, {
+            tite: 'Todo D',
+            project: 'Project D',
+            done: false,
+         }],
+      };
+   },
 };
 </script>
 
@@ -21,7 +41,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <p>Completed Tasks: {{ todos.filter(todo => {return todo.done === true}).length }}</p>
+      <p>Pending Tasks: {{ todos.filter(todo => {return todo.done === false}).length }}</p>
+      <todo v-for="todo in todos" :key="todo" v-bind:todo="todo"></todo>
+  </div>
+</template>
+
+<script>
+import Todo from './Todo';
+
+export default {
+  props: ['todos'],
+  components: {
+     Todo,
+  },
+};
+</script>
+
+<style>
+
+</style>
